b2b7c6985eb66fd96a6b69dc76b43e61af99a817
HiagoSilva <hiagosilvaanjos7@gmail.com>
2024-06-26 22:08:42 -0300

    chore: add prettier and eslint setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Say Hello World</h1>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div>
+                <h1>Say Hello World</h1>
+            </div>
+        </>
+    );
 }
 
 export default App;
